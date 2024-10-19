3e83058f5669d9f1fea2e337e724c302347ddb97
Roaa2222 <roaa1602@gmail.com>
2024-10-19 11:14:13 -0700

    done

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,14 @@
 -- Use the information_schema to get details about the books table
 SELECT 
     COLUMN_NAME,
-    DATA_TYPE,
+    COLUMN_TYPE,
     CHARACTER_MAXIMUM_LENGTH,
     IS_NULLABLE,
     COLUMN_DEFAULT,
     COLUMN_KEY,
     EXTRA
 FROM 
-    information_schema.columns
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    table_schema = 'alx_book_store' AND 
-    table_name = 'Books';
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'Books';
