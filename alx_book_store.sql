2d2d03e4ec2408cca96e0eb8c6cb0b6dd8620892
Roaa2222 <roaa1602@gmail.com>
2024-10-19 08:59:51 -0700

    done

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,16 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the newly created database
+USE alx_book_store;
+
+-- Create the Authors table
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
+-- Create the Books table
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -12,6 +20,7 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
+-- Create the Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -19,6 +28,7 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
+-- Create the Orders table
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -26,6 +36,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
+-- Create the Order_Details table
 CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
